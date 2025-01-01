7bb8be90f9398cf9d6c8170b8f27e705dfb51999
yenrongchen <yenrong1040130@gmail.com>
2025-01-01 16:07:14 +0800

    干擾器

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MonsterController : MonoBehaviour
 {
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -15,4 +18,9 @@ public class MonsterController : MonoBehaviour
     {
         
     }
+
+    //if (enemy_detected)
+    //{
+    //    animator.setInteger("state", 1);
+    //}
 }
