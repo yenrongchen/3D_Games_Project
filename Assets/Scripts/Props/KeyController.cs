294c54b6ef17905fbf7ac9aba2c88a20d45cab91
yenrongchen <yenrong1040130@gmail.com>
2025-01-12 17:29:20 +0800

    微調鑰匙動畫

--- Assets/Scripts/Props/KeyController.cs
+++ Assets/Scripts/Props/KeyController.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
-    public float rotateAngle = 60f;
-
+    public int ID = 1;
+    private float rotateAngle = 60f;
+    private float speed = 0.08f;
     private float timer = 4f;
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(new Vector3(0f, 0f, rotateAngle * Time.deltaTime));
-
-        if (timer > 2f)
+        if (ID == 1)
         {
-            this.transform.position += new Vector3(0f, 0.05f * Time.deltaTime, 0f);
+            this.transform.Rotate(new Vector3(0f, 0f, rotateAngle * Time.deltaTime));
         }
         else
         {
-            this.transform.position -= new Vector3(0f, 0.05f * Time.deltaTime, 0f);
+            this.transform.Rotate(new Vector3(0f, rotateAngle * Time.deltaTime, 0f));
+        }
+        
+        if (timer > 2f)
+        {
+            this.transform.position += new Vector3(0f, speed * Time.deltaTime, 0f);
+        }
+        else
+        {
+            this.transform.position -= new Vector3(0f, speed * Time.deltaTime, 0f);
         }
 
         timer -= Time.deltaTime;
+
         if (timer <= 0f)
         {
             timer = 4f;
